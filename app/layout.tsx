9df4a024f17e4ea33604f889ee6e509e3b86b0cb
cedric <cedric-marchal-professional@proton.me>
2026-02-10 15:31:14 +0100

    chore: update robots meta tags to prevent indexing and following
    - Modified the robots meta tags in layout.tsx to set both index and follow to false, preventing search engines from indexing the site and following links.
    - Ensured consistency in the robots settings for Googlebot as well.

--- app/layout.tsx
+++ app/layout.tsx
@@ -73,11 +73,11 @@ export const metadata: Metadata = {
     // site: "@votrecompte",
   },
   robots: {
-    index: true,
-    follow: true,
+    index: false,
+    follow: false,
     googleBot: {
-      index: true,
-      follow: true,
+      index: false,
+      follow: false,
       "max-video-preview": -1,
       "max-image-preview": "large",
       "max-snippet": -1,
